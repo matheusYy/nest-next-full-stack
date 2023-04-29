ce0568f438b4683b26bd6e040a8f774d949b99f7
Matheus R <matheusrodrigues.yy7@gmail.com>
2023-04-28 22:50:18 -0300

    resolv build

---
+++ .next/static/a-VZo99cM0qv8dSYBjLb8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-db41f0f1cce9ffcb.js"],"/Home":["static/chunks/pages/Home-7cdde65b70921136.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/Home","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ .next/static/chunks/pages/Home-7cdde65b70921136.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[336],{8381:function(n,u,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Home",function(){return e(608)}])},608:function(n,u,e){"use strict";e.r(u);var _=e(5893);let t=()=>(0,_.jsx)("h1",{children:"home"});u.default=t}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8381)}),_N_E=n.O()}]);
